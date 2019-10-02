99d3a8422c26d5f7ad6eed6e81a68b495d16ad75
MushBonger <halloran@mymail.mines.edu>
2019-10-02 16:00:05 -0600

    Changed eye height from 20 to 12

--- DrawPlay/src/Cat.java
+++ DrawPlay/src/Cat.java
@@ -11,7 +11,7 @@ public class Cat {
 	private static final int EYE_X = HEAD_DIMENSION/4;
 	private static final int EYE_SEPARATION = HEAD_DIMENSION/3;
 	// pick eye dimensions
-	private static final int EYE_HEIGHT = 20;
+	private static final int EYE_HEIGHT = 12; //changed eye height from 20 to 12
 	private static final int EYE_WIDTH = 10;
 	// pick mouth height, width is based on head dimension
 	private static final int MOUTH_HEIGHT = 10;
